52d5eb2ebe6adf5d3a938aa0e44887e132f2a38a
dyzzbao <1095238893@qq.com>
2020-05-25 23:33:02 +0800

    设置字体大小不随着适配缩放而缩放

--- music/src/main.js
+++ music/src/main.js
@@ -2,7 +2,11 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-
+// 借助fastclick解决移动端100~300ms的点击事件延迟问题
+import fastclick from 'fastclick'
+// 初始化默认全局样式 
+import './assets/css/base.scss'
+fastclick.attach(document.body)
 Vue.config.productionTip = false
 
 new Vue({
