c192116b7e27de0f5a77da24f4c4ad133ca7249c
dyzzbao <1095238893@qq.com>
2020-05-25 21:11:08 +0800

    初始化配置，1、适配大部分浏览器。2、加载html-loader。3、postcss-pxtorem自动px转rem。4、实现浏览器兼容

--- music/src/App.vue
+++ music/src/App.vue
@@ -1,7 +1,14 @@
 <template>
 	<div id="app">
+		<div class="test"></div>
 	</div>
 </template>
 
 <style lang="scss">
+.test {
+	width: 100px;
+	height: 100px;
+	background: #f00;
+	transform: translate(100px, 100px);
+}
 </style>
---
+++ music/vue.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  // 使用html-loader,并压缩
+  configureWebpack: {
+    module: {
+      rules: [
+        {
+          test: /\.(html)$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'html-loader',
+            options: {
+              minimize: true
+            }
+          }
+        }
+      ]
+    }
+  }
+}
